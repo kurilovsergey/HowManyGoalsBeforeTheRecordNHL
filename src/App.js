8e3fc75211d8a60749313f2269082af3227d0fa7
kurilovsergey <kurilovsergey7@gmail.com>
2021-12-09 21:53:30 +0300

    add multi-language and other fix

--- src/App.js
+++ src/App.js
@@ -14,8 +14,17 @@ function App() {
   let startGames = 1197;
   let CurrentSeason = "20212022"
   
+  
+  let isRuss = false;
 
   useEffect(()=> {
+  
+  //опрелеяю язык браузера
+  if ((navigator.language || navigator.userLanguage).includes('ru')) { isRuss = true }
+  
+  // console.log('lang = ',lang.includes('ru'))
+
+
   axios.get('https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=statsSingleSeason&season='+CurrentSeason)
   .then(function (response) {
     setOviGoals(startGoal+response.data.stats[0].splits[0].stat.goals)
@@ -26,7 +35,11 @@ function App() {
   axios.get('https://ipapi.co/json/')
   .then((resp) => {
     let data = resp.data;
-
+    //console.log(data.country)
+    //data.country=="RU" ? setENG(false) : null
+    //console.log(data)
+    setIsEng(true)
+    console.log(isEng)
 })
 
 axios.get('https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=gameLog&season='+CurrentSeason)
@@ -46,18 +59,28 @@ axios.get('https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=gameLo
   let [goalOvi, setOviGoals] = useState(null)
   let [gamesOvi, setOviGames] = useState(null)
   let [LastGoal, setDateLastGoal] = useState({date: null,  goals: null})
+  let [isEng, setIsEng] = useState(true)
+  
   
   let difference = goalGretsky - goalOvi
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+      </header>
+      <main className="App-header">
         <Logo/>
-        <Counter difference={difference}/>
+        <Counter isRuss={isRuss} difference={difference}/>
         <HockeyPunk difference={difference}/>
         <StatTable goalOvi={goalOvi} gamesOvi={gamesOvi}/>
         <ScoresToday LastGoal={LastGoal}/>
-      </header>
+      </main>
+      <footer>
+      <hr/>
+        <div><a href="https://www.instagram.com/kurilovsergey/">Made by Kurilov Sergey ❄️</a></div>
+        <div>For partnership agreement, development and marketing write to kurilovsergey7@gmail.com</div>
+      
+      </footer>
     </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -4,20 +4,30 @@ import s from './Counter.module.css'
 
 export const Counter = (props) => {
     const [num, setNum] = useState(props.difference);
+    const [leng, setLeng] = useState(props.isEng);
     
     useEffect(() => {
       setNum(props.difference)},
       [props.difference]
     );
+
+    useEffect(() => {
+      setLeng(props.isEng)},
+      [props.isEng]
+    );
+
+    console.log(leng)
     
  return (
   <div className={s.counter}>
   <h1>
-    Ovechkin needs 
+    {props.isRuss ? "Ovechkin needs" : "Овечкину нужно забить"}
   </h1>
     <div className={s.flip}><Flip  value={num} /></div>
   <h1>
-    goals to climb NHL all time high scoring record 
+   {props.isRuss ? "goals to climb NHL all time high scoring record" 
+   : "шайб до звание лучшего снайпера в истории НХЛ"}
+   
   </h1>
   </div>
  )
